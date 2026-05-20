b54ac1231e209484d3760b68825f6e8397043756
Aditi <aditishete2006@gmail.com>
2026-05-20 12:11:26 +0530

    Added JWT authentication and protected routes

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Header
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 
 import models
 from database import SessionLocal
 
+import auth_utils
+print(dir(auth_utils))
+from auth_utils import hash_password, verify_password, create_access_token
+from jose import jwt
+
 app = FastAPI()
 
-# Pydantic model
+# ---------------- SCHEMAS ---------------- #
+
 class PostCreate(BaseModel):
     title: str
     body: str
     userId: int
 
-# DB dependency
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+# ---------------- DB ---------------- #
+
 def get_db():
     db = SessionLocal()
     try:
@@ -21,12 +32,14 @@ def get_db():
     finally:
         db.close()
 
-# TEST ROUTE (IMPORTANT)
+# ---------------- BASIC ROUTE ---------------- #
+
 @app.get("/")
 def root():
     return {"message": "API working"}
 
-# CREATE ROUTE
+# ---------------- CRUD ---------------- #
+
 @app.post("/posts")
 def create_post(post: PostCreate, db: Session = Depends(get_db)):
     db_post = models.Post(**post.dict())
@@ -34,3 +47,44 @@ def create_post(post: PostCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_post)
     return db_post
+
+@app.get("/posts")
+def get_posts(db: Session = Depends(get_db)):
+    return db.query(models.Post).all()
+
+# ---------------- AUTH ---------------- #
+
+@app.post("/signup")
+def signup(user: UserCreate, db: Session = Depends(get_db)):
+    hashed_pw = hash_password(user.password)
+    new_user = models.User(username=user.username, password=hashed_pw)
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User created"}
+
+@app.post("/login")
+def login(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.username == user.username).first()
+
+    if not db_user or not verify_password(user.password, db_user.password):
+        return {"error": "Invalid credentials"}
+
+    token = create_access_token({"sub": db_user.username})
+
+    return {"access_token": token, "token_type": "bearer"}
+
+# ---------------- PROTECTED ROUTE ---------------- #
+
+SECRET_KEY = "mysecretkey"
+
+def get_current_user(token: str = Header()):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+    return payload
+
+@app.get("/protected")
+def protected(user=Depends(get_current_user)):
+    return {"message": "Authorized", "user": user}
+
